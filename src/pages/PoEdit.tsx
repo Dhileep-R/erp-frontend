aa7c0738851089fac6fbc1f8b61a647c2f5771f7
dhileepKumar <dhileepdm@gmail.com>
2026-01-26 21:47:46 +0530

    develop | error message changed.

--- src/pages/PoEdit.tsx
+++ src/pages/PoEdit.tsx
@@ -47,7 +47,7 @@ export default function PoEdit() {
       }
       navigate('/');
     } catch {
-      setError('Failed to save PO');
+      setError('Failed to save POsssss');
     } finally {
       setSaving(false);
     }
